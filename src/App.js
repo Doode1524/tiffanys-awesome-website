64a5130904faa1a238af8831812955551c5eb345
joey <doode1524@gmail.com>
2022-01-28 16:25:11 -0500

    gh pages dependancy

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>
+         THIS IS TIFFANY'S AWESOME FUCKING WEBSITE!!!
+        </h3>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/tiffany.stavros"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Meet Tiff
         </a>
       </header>
     </div>
