f8749697bc6d0f250f04eedd3161817176045471
joey <doode1524@gmail.com>
2022-01-30 15:29:59 -0500

    header init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>
-         THIS IS TIFFANY'S AWESOME FUCKING WEBSITE!!!
-        </h3>
-        <a
-          className="App-link"
-          href="https://www.facebook.com/tiffany.stavros"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Meet Tiff
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
